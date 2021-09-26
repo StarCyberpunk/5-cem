6eb3dfa5b9588bf3ed4c5d26e386e077b2663897
Emil <Starostin.emil@mail.ru>
2021-09-26 20:08:24 +0300

    Change

--- Optimum/Program.cs
+++ Optimum/Program.cs
@@ -9,7 +9,7 @@ namespace Optimum
         {
             Console.WriteLine("Ответ шаговый {0}",Extremum.Shagoviy(1,0.5,0.0001,x=>(x*x/2)+(8/(x*x))));
             Console.WriteLine("Золотое сечение {0}",Extremum.GoldSechenie(1, 5, 0.0001, x => (x * x / 2) + (8 / (x * x))));
-            Console.WriteLine("Квад Апрокси {0}", Extremum.MetodKvadAproksim(1,2,5, 0.0001, x => (x * x)));
+            Console.WriteLine("Квад Апрокси {0}", Extremum.MetodKvadAproksim(-5,0,5, 0.0001, x => x * x+5*x));
         }
     }
 }
--- Optimum/Vector.cs
+++ Optimum/Vector.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Optimum
+namespace ConsoleTestVectorMatrix
 {
     class Vector
     {
