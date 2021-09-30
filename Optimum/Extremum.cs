d96c197aac9a47b9d6e80bc0ca04014aea9808d2
Emil <Starostin.emil@mail.ru>
2021-09-30 17:33:57 +0300

    27.09
    +gradient

--- Optimum/Extremum.cs
+++ Optimum/Extremum.cs
@@ -5,26 +5,27 @@ using System.Text;
 namespace Optimum
 {
     delegate double Fun(double x);
+    delegate double Fun2(Vector x);
     class Extremum
     {
-        public static double Shagoviy(double xn,double h,double eps, Fun fun) 
+        public static double Shagoviy(double xn, double h, double eps, Fun fun)
         {
             int k = 1;
             double fn = fun(xn);
             double xs = xn + h;
-            double fs = fn+fun(xs);
-            while (Math.Abs(h)>eps)
+            double fs = fn + fun(xs);
+            while (Math.Abs(h) > eps)
             {
                 if (fs > fn)
                 {
-                    h =- h / 2;
-                   
+                    h = -h / 2;
+
                 }
                 else
                 {
-                    
-                    h=h*1.2;
-                    
+
+                    h = h * 1.2;
+
                 }
                 xn = xs;
                 fn = fs;
@@ -35,10 +36,10 @@ namespace Optimum
             Console.WriteLine("k={0}", k);
             return xn;
         }
-        public static double GoldSechenie(double a,double b,double eps, Fun fun)
+        public static double GoldSechenie(double a, double b, double eps, Fun fun)
         {
             int k = 2;
-            double fi= (Math.Sqrt(5) - 1) / 2;
+            double fi = (Math.Sqrt(5) - 1) / 2;
             double l = b - a;
             double v = a + l * (1 - fi);
             double w = a + l * fi;
@@ -52,7 +53,7 @@ namespace Optimum
                     w = v;
                     fw = fv;
                     l = b - a;
-                    v= a + l * (1 - fi);
+                    v = a + l * (1 - fi);
                     fv = fun(v);
                 }
                 else
@@ -69,7 +70,7 @@ namespace Optimum
             Console.WriteLine("k={0}", k);
             return (a + b) / 2;
         }
-        public static double MetodKvadAproksim(double x1, double x2, double x3,double eps,Fun fun)
+        public static double MetodKvadAproksim(double x1, double x2, double x3, double eps, Fun fun)
         {
             double xpred = DopolenieMedoda(x1, x2, x3, fun);
             double xtek = 0;
@@ -107,8 +108,51 @@ namespace Optimum
             double fm = xpred * xpred * c + b * xpred + a;
             return xpred;
         }
+        public static Vector Grad(Vector xn, double eps, Fun2 func, double h) {
+            Vector xnach = new Vector(xn);
+            int k = 2;
+            int n = xn.Size;
+            double fn = func(xn);
+            Vector xs = new Vector(xn);
+            Vector gr;
+            Vector dx;
+            double delta = 0.5 * eps;
+            double fs = fn + func(xs);
+            do
+            {
+                gr = new Vector(n);
+                for (int i = 0; i < n; i++)
+                {
+                    /*Vector t = new Vector(1);
+                    t[0] = xn[i];
+                    Vector delta2 = new Vector(1);
+                    delta2[0] = delta+xn[i];
+                    gr[i+1] = (func(delta2)-func(t))/delta;*/
+                    Vector xg = xn.Copy();
+                    xg[i] = xg[i] + delta;
+                    gr[i] = (func(xg) - fn) / delta;
+                }
+                dx= -h * gr;
+                xs = xn - h * gr;
+                fs = func(xs);
+                if (fs > fn)
+                {
+                    h = -h / 2;
+                }
+                else
+                {
+                    h = 1.2 * h;
 
+                }
+                xn = xs;
+                fn = fs;
+                k++;
 
+            }
+            while (Math.Abs(dx.NormaE()) > eps);
+            Console.WriteLine(k);
+            return xs;
 
     }
+    } 
 }
--- Optimum/Program.cs
+++ Optimum/Program.cs
@@ -10,6 +10,9 @@ namespace Optimum
             Console.WriteLine("Ответ шаговый {0}",Extremum.Shagoviy(1,0.5,0.0001,x=>(x*x/2)+(8/(x*x))));
             Console.WriteLine("Золотое сечение {0}",Extremum.GoldSechenie(1, 5, 0.0001, x => (x * x / 2) + (8 / (x * x))));
             Console.WriteLine("Квад Апрокси {0}", Extremum.MetodKvadAproksim(-5,0,5, 0.0001, x => x * x+5*x));
+            Vector xn = new Vector(2);
+            xn[0] = 1;xn[1] = 1;
+            Console.WriteLine("Градиент {0}", Extremum.Grad(xn,0.001, x => x[0]*x[0]+x[1]*x[0]+2*x[1]*x[1],0.1));
         }
     }
 }
